52be1df65a9591111dc19c31ebdfe17299e83b32
Isabella Fernandes <149580770+Isabellaftavares@users.noreply.github.com>
2024-06-18 11:03:27 +0000

    add `pages/index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>TESTE</h1>
+    return <h1>A dor da disciplina é temporária. A vida retribui quem escolhe o desconforto no curto prazo pelos ganhos a longo prazo!!</h1>
 }
 
-export default Home;
+export default Home; 
