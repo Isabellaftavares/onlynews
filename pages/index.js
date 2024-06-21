61c7a11f933791f213f00e021af7d3f2184b16bc
Isabella Fernandes <149580770+Isabellaftavares@users.noreply.github.com>
2024-06-21 10:18:51 +0000

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>A dor da disciplina é temporária. A vida retribui quem escolhe o desconforto no curto prazo pelos ganhos a longo prazo!!</h1>
+  return (
+    <h1>
+      A dor da disciplina é temporária. A vida retribui quem escolhe o
+      desconforto no curto prazo pelos ganhos a longo prazo!!
+    </h1>
+  );
 }
 
-export default Home; 
+export default Home;
